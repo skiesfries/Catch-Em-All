452eeb3158f61eb49b8a403fe20b6854c7819f65
Teresa Ortiz <tortiz942@gmail.com>
2023-01-03 17:29:10 -0500

    initial schema set up for basic products on sanity
    set up sanity schemas for sealedproducts and card products as well as a banner that will go on the frontpage and in product detail pages

---
+++ main/catchemall/sanity_CEA/schemas/index.js
@@ -0,0 +1,5 @@
+import banner from './banner';
+import cardSingles from './cardSingles';
+import sealedProducts from './sealedProducts';
+
+export const schemaTypes = [banner, cardSingles, sealedProducts]
